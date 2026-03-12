40baa437772acaec34c80fa03211805e636b7771
ologh1556 <ologh1556@gmail.com>
2026-03-12 19:38:57 +0200

    modify .gitignore

---
+++ clases/enemy.cpp
@@ -0,0 +1,5 @@
+//
+// Created by gigabyte on 12.03.2026.
+//
+
+#include "enemy.h"
---
+++ clases/enemy.h
@@ -0,0 +1,16 @@
+//
+// Created by gigabyte on 12.03.2026.
+//
+
+#ifndef LAB_1_ENEMY_H
+#define LAB_1_ENEMY_H
+
+
+
+class enemy {
+
+};
+
+
+
+#endif //LAB_1_ENEMY_H
