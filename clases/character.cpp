63e9a153d4a30cfb1e8d6b8fe37e90d89700c4f4
ologh1556 <ologh1556@gmail.com>
2026-03-12 19:51:11 +0200

    initialize class character

--- clases/character.cpp
+++ clases/character.cpp
@@ -1,3 +1,53 @@
+#include "Character.h"
+#include <iostream>
 
+// delegation constructor
+Character::Character() : Character("Hero", 100, 1) {
+    std::cout << "delegation constructor called\n";
+}
 
-#include "character.h"
+// constructor with args
+Character::Character(std::string n, int h, int lvl)
+    : name(n), health(h), level(lvl) {
+    characterCount++;
+}
+
+// copy constructor
+Character::Character(const Character& other)
+    : name(other.name), health(other.health), level(other.level) {
+    characterCount++;
+}
+
+// move constructor
+Character::Character(Character&& other) noexcept
+    : name(other.name), health(other.health), level(other.level) {
+
+    other.name = "";
+    other.health = 0;
+    other.level = 0;
+}
+
+// destructor
+Character::~Character() {
+    std::cout << "Character destroyed: " << name << std::endl;
+    characterCount--;
+}
+
+void Character::showInfo() const {
+    std::cout << name << " HP:" << health << " Level:" << level << std::endl;
+}
+
+int Character::characterCount = 0;
+
+int Character::getCharacterCount() {
+    return characterCount;
+}
+
+Character& Character::operator+=(int hp) {
+    health += hp;
+    return *this;
+}
+
+bool Character::operator!() const {
+    return health <= 0;
+}
--- clases/character.h
+++ clases/character.h
@@ -1,8 +1,26 @@
 #pragma once
+#include <string>
 
-class character
-{
+class Character {
+private:
+    std::string name;
+    int health;
+    int level;
+
+    static int characterCount;
+
+public:
+    Character();
+    Character(std::string n, int h, int lvl);
+
+    Character(const Character& other);
+    Character(Character&& other) noexcept;
+
+    ~Character();
+
+    void showInfo() const;
+    int getCharacterCount();
+
+    Character& operator+=(int hp);
+    bool operator!() const;
 };
-
-
-#endif //LAB_1_CHARACTER_H
