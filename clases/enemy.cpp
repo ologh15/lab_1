153b662ee17686963c5780d029f097e1f7f7d4a6
ologh1556 <ologh1556@gmail.com>
2026-03-15 15:23:25 +0200

    changed class enemy

--- clases/enemy.cpp
+++ clases/enemy.cpp
@@ -1,15 +1,28 @@
-#include "Enemy.h"
+#include "enemy.h"
 #include <iostream>
 
-Enemy::Enemy() : Enemy("Goblin", 15) {}
+Enemy::Enemy() : type("Unknown"), damage(0) {}
 
-Enemy::Enemy(std::string t, int d)
-    : type(t), damage(d) {}
+Enemy::Enemy(std::string t, int d) : type(t), damage(d) {}
 
 Enemy::~Enemy() {
-    std::cout << "Enemy destroyed\n";
+    std::cout << "Enemy destroyed: " << type << std::endl;
 }
 
 void Enemy::showInfo() const {
-    std::cout << "Enemy: " << type << " Damage: " << damage << std::endl;
+    std::cout << "Enemy: " << type << ", Damage: " << damage << std::endl;
+}
+
+bool Enemy::operator!() const {
+    return damage <= 0;
+}
+
+std::ostream& operator<<(std::ostream& out, const Enemy& enemy) {
+    out << enemy.type << " " << enemy.damage;
+    return out;
+}
+
+std::istream& operator>>(std::istream& in, Enemy& enemy) {
+    in >> enemy.type >> enemy.damage;
+    return in;
 }
--- clases/enemy.h
+++ clases/enemy.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <string>
+#include <iostream>
 
 class Enemy {
 private:
@@ -9,8 +10,11 @@ private:
 public:
     Enemy();
     Enemy(std::string t, int d);
-
     ~Enemy();
 
     void showInfo() const;
+    bool operator!() const;
+
+    friend std::ostream& operator<<(std::ostream& out, const Enemy& enemy);
+    friend std::istream& operator>>(std::istream& in, Enemy& enemy);
 };
