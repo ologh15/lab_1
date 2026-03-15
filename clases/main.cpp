bb0cafd303710ca7d0ad2af9a65e7a6fd20f71da
ologh1556 <ologh1556@gmail.com>
2026-03-15 15:27:53 +0200

    changed main

--- clases/main.cpp
+++ clases/main.cpp
@@ -1,17 +1,31 @@
-#include "Character.h"
-#include "Item.h"
-#include "Enemy.h"
+#include <iostream>
+#include <utility>
+#include "character.h"
+#include "item.h"
+#include "enemy.h"
 
 int main() {
 
-    Character hero("Knight", 150, 5);
-    hero.showInfo();
+    Character hero1;
+    Character hero2("Knight", 150, 5);
 
-    Item sword("Sword", 25);
-    sword.showInfo();
+    hero1.showInfo();
+    hero2.showInfo();
 
-    Enemy dragon("Dragon", 50);
-    dragon.showInfo();
+    Item item1("Sword", 25);
+    item1.showInfo();
+
+    Enemy enemy1("Dragon", 50);
+    enemy1.showInfo();
+
+    Character hero3 = hero2;
+    Character hero4 = std::move(hero3);
+
+    hero2 += 20;
+
+    std::cout << hero2 << std::endl;
+
+    std::cin >> hero2;
 
     return 0;
 }
