ea0433e0b943276a39bfce11a3c043ee1442bea9
ologh1556 <ologh1556@gmail.com>
2026-03-15 15:22:21 +0200

    chenged class item

--- clases/item.cpp
+++ clases/item.cpp
@@ -1,15 +1,24 @@
-#include "Item.h"
+#include "item.h"
 #include <iostream>
 
-Item::Item() : Item("Potion", 10) {}
+Item::Item() : name("Unknown"), power(0) {}
 
-Item::Item(std::string n, int p)
-    : name(n), power(p) {}
+Item::Item(std::string n, int p) : name(n), power(p) {}
 
 Item::~Item() {
-    std::cout << "Item destroyed\n";
+    std::cout << "Item destroyed: " << name << std::endl;
 }
 
 void Item::showInfo() const {
-    std::cout << "Item: " << name << " Power: " << power << std::endl;
+    std::cout << "Item: " << name << ", Power: " << power << std::endl;
+}
+
+std::ostream& operator<<(std::ostream& out, const Item& item) {
+    out << item.name << " " << item.power;
+    return out;
+}
+
+std::istream& operator>>(std::istream& in, Item& item) {
+    in >> item.name >> item.power;
+    return in;
 }
--- clases/item.h
+++ clases/item.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <string>
+#include <iostream>
 
 class Item {
 private:
@@ -9,8 +10,10 @@ private:
 public:
     Item();
     Item(std::string n, int p);
-
     ~Item();
 
     void showInfo() const;
+
+    friend std::ostream& operator<<(std::ostream& out, const Item& item);
+    friend std::istream& operator>>(std::istream& in, Item& item);
 };
